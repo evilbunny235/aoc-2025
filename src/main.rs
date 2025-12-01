236d90fd2e51f52ae82863f4faaac7767f171329
evilbunny235 <125089670+evilbunny235@users.noreply.github.com>
2025-12-01 20:14:09 +0100

    finish day 1 puzzle

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,36 @@ fn main() {
     let file = File::open("inputs/day1.txt").unwrap();
     let reader = BufReader::new(file);
 
+    let mut dial: i64 = 50;
+    let mut result = 0;
+
     for line in reader.lines() {
         let line = line.unwrap();
+
+        let (op, n) = line.split_at(1);
+        let n: i64 = n.parse().unwrap();
+
+        match op {
+            "L" => {
+                dial -= n;
+            }
+            "R" => {
+                dial += n;
+            }
+            _ => {
+                panic!("unknown op");
+            }
+        }
+
+        dial = dial % 100;
+        if dial < 0 {
+            dial += 100;
+        }
+
+        if dial == 0 {
+            result += 1;
+        }
     }
+
+    println!("result: {result}");
 }
